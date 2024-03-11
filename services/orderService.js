9b42ec307bc6c6737c2326561c1cbc91f54c653f
amayou21 <amaguaryoussefpay10@gmail.com>
2024-03-11 13:30:02 +0100

    create order model & cash order services

---
+++ services/orderService.js
@@ -0,0 +1,52 @@
+const factory = require("./handleFactory");
+const asyncHandler = require("express-async-handler");
+const cartModel = require("../models/cartModel");
+const orderModel = require("../models/orderModel");
+const ApiError = require("../utility/apiError");
+const ProductModel = require("../models/ProductModel");
+
+
+
+// @desc    create cash order
+// @route   POST api/v1/orders/cartId
+// @access  Protected/User
+exports.crateCashOrder = asyncHandler(async (req, res, next) => {
+
+    // app settings
+    const taxPrice = 0
+    const shippingPrice = 0
+
+    // 1) get cart based on cart ID 
+    const cart = await cartModel.findById(req.params.cartId)
+    if (!cart) {
+        return next(new ApiError(`there is no cart with this ID : ${req.user._id}`, 404))
+    }
+
+    // 2) get order price depend on cart price "check if coupon apply"
+    const cartPrice = cart.totalCartPriceAfterDiscount ? cart.totalCartPriceAfterDiscount : cart.totalCartPrice
+    const totalOrderPrice = cartPrice + taxPrice + shippingPrice
+
+    // 3) create order with default payment method cash
+    const order = await orderModel.create(
+        {
+            user: req.user._id,
+            cartItem: cart.cartItem,
+            shippingAddress: req.body.shippingAddress,
+            totalOrderPrice
+        }
+    )
+    // 4)After creating order, decrement product quantity, increment product sold
+    if (order) {
+        const bulkOption = cart.cartItem.map(item => ({
+            updateOne: {
+                filter: { _id: item.product },
+                update: { $inc: { quantity: -item.quantity, sold: + item.quantity } }
+            },
+        }))
+
+        await ProductModel.bulkWrite(bulkOption, {})
+
+        // 5) clear cart depend on cartid
+        await cartModel.findByIdAndDelete(req.params.cartId)
+    }
+})
